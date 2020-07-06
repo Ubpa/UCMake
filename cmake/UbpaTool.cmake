5261133cef1e42e8def93093fa6ff7e33951fc41
Ubpa <641614112@qq.com>
2020-07-06 16:36:39 +0800

    - natvis

--- cmake/UbpaTool.cmake
+++
@@ -1,5 +0,0 @@
-message(STATUS "include UbpaTool.cmake")
-
-include("${CMAKE_CURRENT_LIST_DIR}/UbpaGit.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/UbpaBuild.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/UbpaDownload.cmake")
