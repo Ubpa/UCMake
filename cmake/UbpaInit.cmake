674ba1c3a28c250d313a93b49fde1124d2f00152
ubpa <641614112@qq.com>
2021-01-18 13:53:31 +0800

    give message before error

--- cmake/UbpaInit.cmake
+++ cmake/UbpaInit.cmake
@@ -35,25 +35,25 @@ macro(Ubpa_InitProject)
   
   if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
   # using Clang
+    message(STATUS "Compiler: Clang ${CMAKE_CXX_COMPILER_VERSION}")
     if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "11")
       message(FATAL_ERROR "clang (< 11) not support concept")
       return()
     endif()
-    message(STATUS "Compiler: Clang ${CMAKE_CXX_COMPILER_VERSION}")
   elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+    message(STATUS "Compiler: GCC ${CMAKE_CXX_COMPILER_VERSION}")
     if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "10")
       message(FATAL_ERROR "gcc (< 10) not support concept")
       return()
     endif()
-    message(STATUS "Compiler: GCC ${CMAKE_CXX_COMPILER_VERSION}")
   # using GCC
   elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
   # using Visual Studio C++
+    message(STATUS "Compiler: MSVC ${CMAKE_CXX_COMPILER_VERSION}")
     if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "19.26")
       message(FATAL_ERROR "MSVC (< 1926 / 2019 16.6) not support concept")
       return()
     endif()
-    message(STATUS "Compiler: MSVC ${CMAKE_CXX_COMPILER_VERSION}")
   endif()
 
   message(STATUS "CXX_STANDARD: ${ARG_CXX_STANDARD}")
